a91cdd645357aaaa55f1958f1b14fd96a4e33fcd
SaiVenkat168 <saivenkatsalpala@gmail.com>
2023-12-19 23:12:29 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Todo from "./components/todoreact/todo";
-// import Temp from "./components/weather/temp";
+//import Todo from "./components/todoreact/todo";
+ import Weather from "./components/temp";
 
 const App = () => {
   return (
     <>
-      <Todo />
+      <Weather />
     </>
   );
 };
